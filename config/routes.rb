0e23d6dbb6ce492b8029cb25e4bb19575bc2a637
amiblog <reza.am.0425@gmail.com>
2022-01-13 12:18:46 +0000

    プロフィール画像追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  resources :users, only: [:show, :edit, :update
+  ]
+  #get 'post_images/new'
+  #get 'post_images/index'
+  #get 'post_images/show'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
